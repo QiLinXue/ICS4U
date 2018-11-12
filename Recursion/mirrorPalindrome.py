38c0afcb86058f00f56d63931585f0718154a414
QiLinXue <645367@pdsb.net>
2018-11-12 10:56:52 -0500

    update

--- Recursion/mirrorPalindrome.py
+++ Recursion/mirrorPalindrome.py
@@ -5,91 +5,47 @@
 #
 # Author:      QiLin
 # Created:     6-Nov-2018
-# Updated:     8-Nov-2018
+# Updated:     12-Nov-2018
 #-----------------------------------------------------------------------------
 
-selfReflection = ['i', 'l', 'm', 'n', 'o', 't', 'u', 'v', 'w', 'x']
-doubleReflection = [('b','d'),('p','q'),('s','z')]
+m = lambda s,i=0,z=2: 0 if(z>len(s) and not len(s)==1) else 1 if s in list("ilmnotuvwx") else 0 if len(s)==1 else m(s,0,z+1) if i+z > len(s) else m(s,i+1,z)+1+(len(__import__('re').findall("(i|[l-o]|[t-x])",s)) if(z==2 and i==0) else 0) if(s.lower()[i:i+z][:len(s.lower()[i:i+z])//2].translate(str.maketrans("bdpqsz","dbqpzs"))[::-1]==s.lower()[i:i+z][__import__('math').ceil(len(s.lower()[i:i+z])/2):] and (len(s.lower()[i:i+z])%2==1 or s.lower()[i:i+z][len(s.lower()[i:i+z])//2] in list("ilmnotuvwx"))) else m(s,i+1,z)+(len(__import__('re').findall("(i|[l-o]|[t-x])",s)) if(z==2 and i==0) else 0)
 
-def recursiveCalculate(string,index=0,size=2):
+import math
+def recursiveCalculate(s,original="",taken=None):
   '''
-  Function to count the number of mirro palindromes within the given string
+  Function to count the number of mirrordrome within the given string
+
   Parameters
   ----------
-  string: str
-    the string to search for palindromes
-  index: int
-    the starting index of each substring
-  size: int
-    the size of each substring
+  s: str
+    the substring of the original to test if it is a palindrome
+  original: str
+    the original string
+  taken: str list
+    a list of substrings that have successfully been tested as a mirrordrome
   
   Returns
   -------
   int:
     the number of palindromes within the given string
+
   '''
+  if taken == None: taken, original = [], s # Run first time
+  if len(s) == 0: return 0 # Base Case
+
+  # Flip First Half
+  firstHalf = s[:len(s)//2].translate(str.maketrans("bdpqsz","dbqpzs"))[::-1]
+  secondHalf = s[math.ceil(len(s)/2):]
 
   n = 0
-
-  if size == 2 and index == 0:
-
-    for rep in doubleReflection: string = string.replace(rep[0], rep[1])
-    for letter in string:
-      if letter in selfReflection:
-        n = n + 1
-    
-  
-  cutString = string.lower()[index:index+size]
-
-  if len(string) == 1:
-    return 1 # If string is only one character
-  if size > len(string):
-    return 0 # Base Case
-  if index+size > len(string):
-    return recursiveCalculate(string,0,size+1) # Reloop with greater substring size
-
-  if isMirrorDrome(cutString):
-    return recursiveCalculate(string,index+1,size) + 1 + n # Next substring if palindrome
-  else:
-    return recursiveCalculate(string,index+1,size) + n # Next substring if no palindrome
-
-def isMirrorDrome(string):
-  '''
-  Function to check if string is a mirror drome
-  ----------
-  string: str
-    the string to check if it's a mirror drome
-  
-  Returns
-  -------
-  bool:
-    True if mirrordrome
-    False if not
-  '''
-  length = len(string)
-  if length % 2 == 0: isOdd = False 
-  else: isOdd = True
-
-  firstHalf = string[:length//2]
-  secondHalf = string[1+length//2:]
-
-  for rep in doubleReflection:
-    if firstHalf == firstHalf.replace(rep[0], rep[1]):
-      firstHalf = firstHalf.replace(rep[1], rep[0])
+  if not original.count(s) <= taken.count(s) and firstHalf == secondHalf:
+    n = 1
+    if len(s) % 2 == 1 and s[len(s)//2] not in list("ilmnotuvwx"):
+      n = 0
     else:
-      firstHalf = firstHalf.replace(rep[0], rep[1])
-
-
-  if firstHalf[::-1] == secondHalf:
-    if isOdd:
-      if string[length//2] in selfReflection:
-        return True
-      else:
-        return False
-    
-    return True
-
-  return False
+      taken.append(s)
+  
+  return recursiveCalculate(s[:len(s)-1],original,taken) + recursiveCalculate(s[1:],original,taken) + n
 
 ##############################################################################
 # Test Function
@@ -100,23 +56,25 @@ import unittest
 testCases = [
   ("dob",2),
   ("linnil",9),
-  ("totally",7)
+  ("totally",7),
+  ("sxz",2)
 ]
 
 class TestMaxPalindrome(unittest.TestCase):
   '''
-  Class to test the max palindrome function
+  Class to test the mirror palindrome function
   '''
   def test_palindrome(self):
     '''
-    Procedure to check for errors in recursion calculator
-    
-    1. Loop through all cases of testCases and check if #of palindromes match
-    ------------
-    Verification
-    2. Verify no errors occured
+    Procedure to check for errors in proper recursion calculator
     '''
     for case in testCases:
       self.assertEqual(recursiveCalculate(case[0]),case[1])
 
+  def test_oneliner(self):
+    '''
+    Procedure to check for errors in one liner mirror palindrome
+    '''
+    for case in testCases:
+      self.assertEqual(m(case[0]),case[1])
 unittest.main()
